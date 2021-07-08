347212073ea33c94b2450e565d3b90cefb219955
Tolik22869 <ggamer556@mail.ru>
2021-07-08 17:42:19 +0300

    mobile adaptive promo section

---
+++ src/js/modules/menu.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function openMenu(selectorTrigger, menuSelector) {
+    const trigger = document.querySelector(selectorTrigger),
+          menu = document.querySelector(menuSelector);
+    
+    trigger.addEventListener('click', ()=> {
+
+    });
+}
